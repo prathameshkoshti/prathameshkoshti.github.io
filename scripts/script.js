6a58dd6b439fac93c5d11231a192f61cfc91a77f
Prathamesh <prathu48@gmail.com>
2019-12-31 11:33:29 +0530

    Final site ready

---
+++ scripts/script.js
@@ -0,0 +1,63 @@
+const typedText = document.getElementById('text');
+const cursor = document.getElementById('cursor');
+const texts = [
+	"Hi, I'm Prathamesh.",
+	"I'm a web dev.",
+	"I'm a ui/ux designer.",
+];
+
+const typingTime = 200;
+const deletingTime = 50;
+const haltTime = 2000;
+const newTextTypingTime = 700;
+
+let currentChar = 0;
+let currentText = 0;
+
+const typeText = () => {
+	if (currentChar <= texts[currentText].length) {
+		if (!cursor.classList.contains('typing')) cursor.classList.add('typing');
+		typedText.textContent += texts[currentText].charAt(currentChar);
+		currentChar++;
+		setTimeout(typeText, typingTime);
+	} else {
+		if (cursor.classList.contains('typing')) cursor.classList.remove('typing');
+		setTimeout(deleteText, haltTime);
+	}
+};
+
+const deleteText = str => {
+	if (currentChar > 0) {
+		if (!cursor.classList.contains('typing')) cursor.classList.add('typing');
+		typedText.textContent = texts[currentText].substr(0, currentChar - 1);
+		currentChar--;
+		setTimeout(deleteText, deletingTime);
+	} else {
+		if (cursor.classList.contains('typing')) cursor.classList.remove('typing');
+		currentText++;
+		if (currentText >= texts.length) currentText = 0;
+		setTimeout(typeText, newTextTypingTime);
+	}
+};
+
+document.addEventListener('DOMContentLoaded', () => setTimeout(typeText, 8000));
+
+const quickLinks = document.querySelector('#checkbox');
+const quickLinksBtn = document.querySelector('#quickLinksBtn');
+
+const toggleLinks = () => {
+	quickLinksBtn.classList.remove('fadein');
+	quickLinksBtn.classList.add('fadeout');
+	if (quickLinks.checked) {
+		setTimeout(() => {
+			quickLinksBtn.classList.remove('fadeout', 'fa-external-link-alt');
+			quickLinksBtn.classList.add('fadein', 'fa-times');
+		}, 300);
+	} else {
+		setTimeout(() => {
+			quickLinksBtn.classList.remove('fadeout', 'fa-times');
+			quickLinksBtn.classList.add('fadein', 'fa-external-link-alt');
+		}, 300);
+	}
+};
+quickLinks.addEventListener('change', toggleLinks);
